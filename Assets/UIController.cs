3cfe58d9880c738e14d3f54c8f20100a4a85bcf8
AlvaroCarbo <alvaro.carbo.7e4@itb.cat>
2021-11-11 16:14:00 +0100

    Player Script WIP
    Jump and movement implementation

---
+++ Assets/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour 
+{
+    Text distanceText;
+    Player player;
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        distanceText = GameObject.Find("distanceText").GetComponent<Text>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int distance = (int)player.distance;
+        distanceText.text = distance + " m";
+    }
+}
