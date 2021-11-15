60228ae643aa486b8257280400de3dab6727e8da
AitorCotano <aitor.cotano.7e4@itb.cat>
2021-11-15 15:26:41 +0100

    WIP Death + Result Panel

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour 
 {
     Text distanceText;
     Player player;
+
+    GameObject results;
+    GameObject distGO;
+
+    Text finalDistanceText; 
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
-        distanceText = GameObject.Find("distanceText").GetComponent<Text>();
+        distGO = GameObject.Find("distanceText");
+        distanceText = distGO.GetComponent<Text>();
+
+
+        finalDistanceText = GameObject.Find("FinalDistanceText").GetComponent<Text>();
+        results = GameObject.Find("Results");
+        results.SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
@@ -23,5 +35,17 @@ public class UIController : MonoBehaviour
     {
         int distance = (int)player.distance;
         distanceText.text = distance + " m";
+
+        if (player.isDead)
+        {
+            results.SetActive(true);
+            distGO.SetActive(false);
+            finalDistanceText.text = distance + " pts";
+        }
+    }
+
+    public void Retry ()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
