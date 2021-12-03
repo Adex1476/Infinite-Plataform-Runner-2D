a7e55faa7c22724342178eb45ac99fae3747f0ca
Alvaro <alvaro.carrbo.7e4@itb.cat>
2021-12-03 21:32:42 +0100

    UI Implentation model done

--- Assets/Scripts/MenuNavegation.cs
+++ Assets/Scripts/MenuNavegation.cs
@@ -15,7 +15,7 @@ public class MenuNavegation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadMenu();
     }
 
     // Update is called once per frame
@@ -24,15 +24,25 @@ public class MenuNavegation : MonoBehaviour
         buttonsMenu[0].onClick.AddListener(LoadGame);
         buttonsMenu[1].onClick.AddListener(LoadShop);
         buttonsMenu[2].onClick.AddListener(LoadGarage);
-        buttonsMenu[3].onClick.AddListener(LoadSettings);
+        buttonsMenu[3].onClick.AddListener(LoadArchivements);
+        buttonsMenu[4].onClick.AddListener(LoadSettings);
+        buttonsMenu[5].onClick.AddListener(UnloadShop);
+        buttonsMenu[6].onClick.AddListener(UnloadGarage);
+        buttonsMenu[7].onClick.AddListener(UnloadArchivements);
+        buttonsMenu[8].onClick.AddListener(UnloadSettings);
         //buttonsMenu[4].onClick.AddListener(LoadGame);
     }
 
     private void LoadMenu() => canvasMenus[0].SetActive(true);
     private void UnloadMenu() => canvasMenus[0].SetActive(false);
-    private void LoadGame() => SceneManager.LoadScene(1);
+    private void LoadGame() => SceneManager.LoadScene("GameScene");
     private void LoadShop() { UnloadMenu(); canvasMenus[1].SetActive(true); }
+    private void UnloadShop() { canvasMenus[1].SetActive(false); LoadMenu(); }
     private void LoadGarage() { UnloadMenu(); canvasMenus[2].SetActive(true); }
-    private void LoadSettings() { UnloadMenu(); canvasMenus[3].SetActive(true); }
-    private void LoadArchivements() { }
+    private void UnloadGarage() { canvasMenus[2].SetActive(false); LoadMenu(); }
+    private void LoadArchivements() { UnloadMenu(); canvasMenus[3].SetActive(true); }
+    private void UnloadArchivements() { canvasMenus[3].SetActive(false); LoadMenu(); }
+    private void LoadSettings() { UnloadMenu(); canvasMenus[4].SetActive(true); }
+    private void UnloadSettings() { canvasMenus[4].SetActive(false); LoadMenu(); }
+
 }
