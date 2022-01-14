93800d2fe9c6c2026a228d8254a4767758140256
Oriol <oriol.gomis.7e4@itb.cat>
2022-01-14 17:39:23 +0100

    Pause Mute Button Done

---
+++ Assets/Scripts/Settings/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    public Sprite[] img;
+    // Start is called before the first frame update
+    [SerializeField] private Image mutear;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        mutear= GameObject.Find("Mute").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q)){
+            
+            audioSource.mute= !audioSource.mute;
+            
+            
+        }
+    }
+
+    public void mute()
+    {
+        
+        audioSource.mute= !audioSource.mute;
+        if(audioSource.mute==false){
+            mutear.sprite= img[0];
+        }else{
+            mutear.sprite= img[1];
+        }
+    }
+}
---
+++ Assets/Scripts/Settings/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Button mute;
+
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mute.onClick.AddListener(()=>Mute());
+    }
+
+    void Mute(){
+        Debug.Log("mute");
+        
+    }
+    
+}
